fc4e8351803662556603285ef30f04655ef321ff
Oswaldo <rodriosw13@gmail.com>
2025-07-14 11:07:06 -0400

    editing

--- src/pages/Vans/VansLayout.jsx
+++
@@ -1,8 +0,0 @@
-import { NavLink, Outlet, useParams } from 'react-router-dom'
-import { vansData } from '../../../vansData'
-
-export const VansLayout = () => {
-  const params = useParams()
-  const findVan = vansData.find((van) => van.id === params.id)
-  return <section className='vans2'></section>
-}
